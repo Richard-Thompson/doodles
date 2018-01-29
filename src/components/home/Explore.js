ed34a6c507808319d88a45b76a2a0fa52ad81b5d
Richard thompson <rpt@Richards-MacBook-Air.local>
2018-01-29 16:50:11 +0000

    changes to explore section

--- src/components/home/Explore.js
+++ src/components/home/Explore.js
@@ -5,34 +5,36 @@ class Explore extends Component {
     render (){
         return(
             <div className='explore-wrapper container-fluid'>
-            <div className='small-triangle'></div>
                 <div className='row'>
-                    <div className='explore-text col-md-6 col-sm-12'></div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='explore-text col-xl-6 col-md-12 col-sm-12'>
+                        <h2 className='explore-title'>Explore the world of doodles.</h2>
+                        <p className='explore-subheading'>Look through the various types of doodles I have and find your perfect doodle. Don't forget if you have an idea for a doodle get in contact.</p>
+                    </div>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                         <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}
                     </div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                     <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}
                     </div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                     <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}</div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                     <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}
                     </div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                     <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}
                     </div>
-                    <div className='type-of-art-section col-md-3 col-sm-6'>
+                    <div className='type-of-art-section col-xl-3 col-md-4 col-sm-6'>
                     <a className='section-link'></a>
                         <div className='section-overlay'></div>
                         {sectionOverlayCorners()}
